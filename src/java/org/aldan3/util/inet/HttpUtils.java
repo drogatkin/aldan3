2198242afc53c70a486efc6c4bba9c82bb90dfee
dmitriy <dmitriy@mochamail.com>
2024-06-04 20:38:38 -0700

    replaced with standard implementation

--- src/java/org/aldan3/util/inet/HttpUtils.java
+++ src/java/org/aldan3/util/inet/HttpUtils.java
@@ -89,22 +89,11 @@ public class HttpUtils {
 	 * @return decoded result
 	 */
 	public static String urlDecode(String _encoded) {
-		// TODO: rewrite using [i] instead of charAt(i)
-		StringBuffer decoded = new StringBuffer();
-		int len = _encoded.length();
-		for (int i = 0; i < len; ++i) {
-			if (_encoded.charAt(i) == '%' && i + 2 < len) {
-				int d1 = Character.digit(_encoded.charAt(i + 1), 16);
-				int d2 = Character.digit(_encoded.charAt(i + 2), 16);
-				if (d1 != -1 && d2 != -1)
-					decoded.append((char) ((d1 << 4) + d2));
-				i += 2;
-			} else if (_encoded.charAt(i) == '+')
-				decoded.append(' ');
-			else
-				decoded.append(_encoded.charAt(i));
-		}
-		return decoded.toString();
+	    try {
+	      return java.net.URLDecoder.decode(_encoded, java.nio.charset.StandardCharsets.UTF_8.name());
+	    } catch(UnsupportedEncodingException uee) {
+	        throw new RuntimeException("UTF-8 encoding isn't available");
+	    }
 	}
 
 	/** Does HTML encode
